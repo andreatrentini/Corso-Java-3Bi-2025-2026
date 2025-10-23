abd968753f384a94d348e3ec5fe20ff32f6be024
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2025-10-23 06:01:16 +0000

    FEAT: TipiEOperatori

---
+++ TipiEOperatori/TipiEOperatori.java
@@ -0,0 +1,42 @@
+package TipiEOperatori;
+
+public class TipiEOperatori {
+    public static void main(String[] args) {
+        /*
+         * Tipi di variabile
+         * Perché: devo dire al sistema operativo quanta ram assegnare per la variabile
+         * Attenzione che anche gli operatori matematici possono comportarsi in maniera diversa
+         */
+        int nomeVariabile;
+        double altraVariabile;
+        float altroDecimale;
+        boolean veroFalso;
+        char singoloCarattere;
+        String testo;
+
+        nomeVariabile = 4;
+        altraVariabile = 5.2;
+        // Stesso valore per il tipo float: ATTENZIONE alla f finale
+        altroDecimale = 5.2f;
+        veroFalso = false;
+        // Le due operazioni successive sono IDENTICHE!!!!!
+        singoloCarattere = 'a';        
+        singoloCarattere = 97;
+        testo = "Ciao come stai?";
+
+        /*
+         * Operatori aritmetici binari (richiedono due valori)
+         * +: somma
+         * -: sottrazione
+         * *: moltiplicazione
+         * /: divisione sia intera che reale
+         * %: resto della divisione intera
+         * 
+         * Operatori artimetici unari
+         * ++: incrementa di uno il valore della variabile
+         * --: decrementa di uno il valore della variabile
+         */
+
+         
+    }
+}

926a176805a454dc804121d48ba3f7f243e03b1a
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2025-12-11 07:40:29 +0000

    FEAT: lettura file e conversione a double dei prezzi

---
+++ LetturaFileTesto/LetturaFileCSV.java
@@ -0,0 +1,74 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class LetturaFileCSV {
+    public static void main(String[] args) {
+        // Oggetto che ci consente di accedere al buffer di memoria nel quale
+        // il PC memorizza ogni riga letta dal file
+        BufferedReader br;
+        // Oggetto che ci consente di leggere (reader) un file da un disco fisso
+        FileReader fr;
+        try {
+            fr = new FileReader("SupermarketSales2.csv");
+            br = new BufferedReader(fr);
+            
+            String riga;
+            
+            // Conto il numero di righe presenti nel file
+            
+            int nrRighe = 0;
+            while ((riga = br.readLine()) != null) {
+                nrRighe++;
+            }
+            System.out.println("Numero di righe presenti: " + nrRighe);
+            
+            // Chiudo il BufferedReader per rileggere il file da capo.
+            // Quando chiudo il BufferedReader viene chiuso anche il file, 
+            // fr.close() NON SERVE.
+
+            br.close();
+
+            double[] unitPrices = new double[nrRighe - 1];
+
+            fr = new FileReader("SupermarketSales2.csv");
+            br = new BufferedReader(fr);
+
+            // Leggo la prima riga e la memorizzo nella variabile riga
+            riga = br.readLine();
+            // La prima riga del file csv contiene i nomi delle colonne
+            // Memorizzo i nomi delle colonne in un array chiamato nomiColonne
+            String[] nomiColonne = riga.split(",");
+            System.out.println("Colonne presenti nel file:");
+            for (int i = 0; i < nomiColonne.length; i++) {
+                System.out.println(nomiColonne[i]);
+            }
+
+            int contatore = 0;
+                      
+            // Ripeto il blocco di istruzione mentre ho righe compilate nel file
+            while ((riga = br.readLine()) != null) {
+                // Divido i valori presenti nella riga usando la virgola come separatore.
+                // L'array di String datiRiga conterrà nelle singole celle i valori letti
+                // dalla riga.
+                // ATTENZIONE: tutti i dati sono in formato string
+                String[] datiRiga = riga.split(",");
+
+                // Converto datiRiga[5] (contiene i prezzi della merce acquistata) in fomato double
+                Double prezzoRiga = Double.parseDouble(datiRiga[5]);
+                unitPrices[contatore] = prezzoRiga;
+            }  
+            // e' sempre buona pratica chiudere il BufferedReader        
+            // fr.close() NON SERVE.  
+            br.close();
+
+            // Adesso ho un array di double con tutti i prezzi degli articoli
+
+            // Calcolo di min, max, media, moda, mediana, varianza sqm
+            
+        }
+        catch (IOException error) {
+            System.out.println("Errore durante la lettura del file: " + error.getMessage());
+        }
+    }
+}

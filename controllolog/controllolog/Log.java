55cfce3c1ac0d7ad14d139cff5fb7652f2211698
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2026-02-19 08:39:04 +0000

    FEAT: inizio progetto gestione LOG

---
+++ controllolog/controllolog/Log.java
@@ -0,0 +1,42 @@
+package controllolog.controllolog;
+
+/**
+ * Descrizione della classe
+ */
+
+public class Log {
+    // Dichiarazione degli attributi
+    private int id;
+    private String tipoUtente;
+    private String allarmeIDS;
+    
+    // Definizione dei costruttore
+    /**
+     * Costruisce un log a partire da una stringa che contiene id,tipoUtente,allarmeIDS
+     * @param riga
+     * @param separatore
+     */
+    public Log(String riga, String separatore) {
+        String datiGrezzi[] = riga.split(separatore);
+        try {            
+            this.id = Integer.parseInt(datiGrezzi[0]);
+            this.tipoUtente = datiGrezzi[1];
+            this.allarmeIDS = datiGrezzi[2];
+        } catch (Exception e) {
+            this.id = 0;
+            this.tipoUtente = null;
+            this.allarmeIDS = null;
+        }
+    }
+
+    public Log(String riga) {
+        this(riga, ",");
+    }
+
+    public Log(int id, String tipoUtente, String allarmeIDS) {
+        this.id = id;
+        this.tipoUtente = tipoUtente;
+        this.allarmeIDS = allarmeIDS;
+    }
+
+}
